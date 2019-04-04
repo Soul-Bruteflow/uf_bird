1db91df863219f23332f26a5e3ff2eb938a7ed4c
deadbighead <thelarion@gmail.com>
2019-04-04 13:13:33 +0300

    0.1

---
+++ core/src/mvlad/ufbird/States/MenuState.java
@@ -0,0 +1,25 @@
+package mvlad.ufbird.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State{
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
---
+++ core/src/mvlad/ufbird/uf_bird.java
@@ -0,0 +1,39 @@
+package mvlad.ufbird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import mvlad.ufbird.States.GameStateManager;
+import mvlad.ufbird.States.MenuState;
+
+public class uf_bird extends ApplicationAdapter {
+
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	public static final String TITLE = "uf_bird";
+	private GameStateManager gsm;
+	private SpriteBatch spriteBatch;
+
+	@Override
+	public void create() {
+		spriteBatch = new SpriteBatch();
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(spriteBatch);
+	}
+
+	@Override
+	public void dispose() {
+		spriteBatch.dispose();
+	}
+}
